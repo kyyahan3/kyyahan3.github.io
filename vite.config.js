e0f6924e5ce98431e3836637a543f521c5962e11
yahan-ds <794598123@qq.com>
2024-06-11 14:46:56 -0500

    fixed packages issue

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { createStyleImportPlugin } from 'vite-plugin-style-import';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    createStyleImportPlugin({
-      libs: [
-        {
-          libraryName: 'antd',
-          esModule: true,
-          resolveStyle: (name) => `antd/es/${name}/style/index`,
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   css: {
     modules: {
-      localsConvention: 'camelCase',
-    },
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
+      localsConvention: "camelCase",
     },
   },
 });
